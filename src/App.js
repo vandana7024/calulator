63857225610d9b0d497d61fc464677dc45702df3
vandana7024 <vandanadewangan1999@gmail.com>
2021-12-06 10:32:18 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import Header from "./Component/Header";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.value));
+  };
+  const clear = () => {
+    setResult(" ");
+  };
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+  const calculate = () => {
+    setResult(eval(result).toString());
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <span>Calculator</span>
+        <input type="text" className="input_box" value={result} />
+        <div className="buttons">
+          <button className="clear" onclick={clear} id="clear">
+            clear
+          </button>
+          <button value="7" onClick={handleClick}>
+            7
+          </button>
+          <button value="8" onClick={handleClick}>
+            8
+          </button>
+          <button value="9" onClick={handleClick}>
+            9
+          </button>
+          <button onClick={backspace} id="backspace">
+            c
+          </button>
+          <button value="/" onClick={handleClick}>
+            /
+          </button>
+          <tr />
+
+          <button value="4" onClick={handleClick}>
+            4
+          </button>
+          <button value="5" onClick={handleClick}>
+            5
+          </button>
+          <button value="6" onClick={handleClick}>
+            6
+          </button>
+          <button value="*" onClick={handleClick}>
+            *
+          </button>
+          <button value="-" onClick={handleClick}>
+            -
+          </button>
+          <button value="1" onClick={handleClick}>
+            1
+          </button>
+          <button value="2" onClick={handleClick}>
+            2
+          </button>
+          <button value="3" onClick={handleClick}>
+            3
+          </button>
+          <button value="+" onClick={handleClick}>
+            +
+          </button>
+          <button value="." onClick={handleClick}>
+            .
+          </button>
+          <button value="0" onClick={handleClick}>
+            0
+          </button>
+          <button value="=" onClick={calculate}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
